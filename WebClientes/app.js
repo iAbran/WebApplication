a3fb05202c60bbe856068e61cc37596ab99aeb0a
migeabr233 <migeabr9@gmail.com>
2024-08-20 14:34:19 -0400

    first commit

---
+++ WebClientes/app.js
@@ -0,0 +1,25 @@
+function getClientes(done) {
+    fetch("http://localhost:8080/inicio/clientes")
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+getClientes(data => {
+    data.forEach(client => {
+        const tbody = document.querySelector("tbody")
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${client.id}</td>
+            <td>${client.name}</td>
+            <td>${client.edad}</td>
+            <td>${client.fechaNacimiento}</td>
+            <td>${client.telefono}</td>
+            <td>${client.email}</td>
+            <td>${client.direccion}</td>
+        `;
+
+        tbody.appendChild(row);  // Usar appendChild para agregar el fragmento
+    });
+});
